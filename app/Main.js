1b07cf7f8d72e42d48a95efa1b26d7f3412b204c
amanda-delongchamp <amanda.delongchamp@gmail.com>
2023-01-04 11:07:38 -0700

    webpack setup

--- app/Main.js
+++ app/Main.js
@@ -1,2 +1,15 @@
+import { func } from "prop-types";
 import React from "react";
 import { ReactDOM } from "react-dom/client";
+
+function ExampleComponent() {
+  return (
+    <div>
+      <h1>This is our app!</h1>
+      <p>The sky is blue.</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<ExampleComponent />);
